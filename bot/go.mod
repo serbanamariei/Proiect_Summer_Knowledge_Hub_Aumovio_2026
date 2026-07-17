da910db46a6a11ac0a9894adac2b8f80201fd090
serbanamariei <amarieiserban2@gmail.com>
2026-07-17 13:40:05 +0300

    am adaugat functionalitatea de descarcare a video urilor de pe youtube si am mutat docker go de pe 1.24 pe 1.26

--- bot/go.mod
+++ bot/go.mod
@@ -1,10 +1,20 @@
 module bot
 
-go 1.24.4
+go 1.26
 
 require (
 	comun v0.0.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/kkdai/youtube/v2 v2.10.6
+)
+
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/dlclark/regexp2 v1.11.5 // indirect
+	github.com/dop251/goja v0.0.0-20260311135729-065cd970411c // indirect
+	github.com/go-sourcemap/sourcemap v2.1.4+incompatible // indirect
+	github.com/google/pprof v0.0.0-20260302011040-a15ffb7f9dcc // indirect
+	golang.org/x/text v0.35.0 // indirect
 )
 
 replace comun => ../comun
--- bot/main.go
+++ bot/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"bytes"
+	"comun"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"comun"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/kkdai/youtube/v2"
 )
 
 func main() {
@@ -34,12 +37,79 @@ func main() {
 
 	for update := range updates {
 		if update.Message != nil {
-			go proceseazaMesaj(bot, update.Message)
+			go proceseazaRutare(bot, update.Message)
 		}
 	}
 }
 
-func proceseazaMesaj(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+func proceseazaRutare(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	if strings.Contains(message.Text, "youtube.com") || strings.Contains(message.Text, "youtu.be") {
+		proceseazaYouTube(bot, message)
+		return
+	}
+
+	proceseazaScraping(bot, message)
+}
+
+func proceseazaYouTube(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
+	msgAsteptare := tgbotapi.NewMessage(message.Chat.ID, "Se descarca video ul...")
+	bot.Send(msgAsteptare)
+
+	client := youtube.Client{}
+	video, err := client.GetVideo(message.Text)
+	if err != nil {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Eroare la citirea video ului: "+err.Error()))
+		return
+	}
+
+	formats := video.Formats.WithAudioChannels()
+	if len(formats) == 0 {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Nu s a gasit un format valid cu audio"))
+		return
+	}
+
+	format := formats[0]
+
+	stream, _, err := client.GetStream(video, &format)
+	if err != nil {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Eroare la preluarea stream ului"))
+		return
+	}
+	defer stream.Close()
+
+	numeFisier := fmt.Sprintf("video_temp_%d.mp4", message.Chat.ID)
+	file, err := os.Create(numeFisier)
+	if err != nil {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Eroare la salvarea temp ului"))
+		return
+	}
+
+	_, err = io.Copy(file, stream)
+	file.Close()
+
+	defer os.Remove(numeFisier)
+
+	if err != nil {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Eroare la salvarea video ului pe disc"))
+		return
+	}
+
+	info, err := os.Stat(numeFisier)
+	if err == nil && info.Size() > 49*1024*1024 {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Video ul depaseste 50 mb si deci nu poate fi trimis pe Telegram"))
+		return
+	}
+
+	msgVideo := tgbotapi.NewVideo(message.Chat.ID, tgbotapi.FilePath(numeFisier))
+	msgVideo.Caption = "Titlu video:" + video.Title
+
+	_, err = bot.Send(msgVideo)
+	if err != nil {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "Eroare la trimiterea video ului pe Telegram"))
+	}
+}
+
+func proceseazaScraping(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	cerere := comun.CerereInitala{
 		ChatID: message.Chat.ID,
 		URL:    message.Text,
@@ -59,8 +129,18 @@ func proceseazaMesaj(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	textMesaj := raspuns.Mesaj
 	if len(raspuns.Linkuri) > 0 {
 		textMesaj += "\n\nLink-urile gasite:\n"
-		for _, link := range raspuns.Linkuri {
-			textMesaj += "- " + link + "\n"
+
+		limitaAfsare := 15
+		if len(raspuns.Linkuri) < limitaAfsare {
+			limitaAfsare = len(raspuns.Linkuri)
+		}
+
+		for i := 0; i < limitaAfsare; i++ {
+			textMesaj += "- " + raspuns.Linkuri[i] + "\n"
+		}
+
+		if len(raspuns.Linkuri) > 15 {
+			textMesaj += "\n... mai sunt si alte link uri"
 		}
 	}
 
